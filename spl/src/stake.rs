d1d9aaaeef3ec0f014208db2c804b90e6c032491
billythedummy <dhy1996@live.com.sg>
2022-06-11 16:14:11 +0800

    stake authorize

--- spl/src/stake.rs
+++ spl/src/stake.rs
@@ -1,9 +1,63 @@
-use anchor_lang::solana_program::{
-    pubkey::Pubkey,
-    stake::{program::ID, state::StakeState},
+use anchor_lang::{
+    context::CpiContext,
+    solana_program::{
+        account_info::AccountInfo,
+        pubkey::Pubkey,
+        stake::{
+            self,
+            program::ID,
+            state::{StakeAuthorize, StakeState},
+        },
+    },
+    Accounts, Result,
 };
 use borsh::BorshDeserialize;
 use std::ops::Deref;
+
+// CPI functions
+
+pub fn authorize<'info>(
+    ctx: CpiContext<'_, '_, '_, 'info, Authorize<'info>>,
+    stake_authorize: StakeAuthorize,
+    custodian: Option<AccountInfo<'info>>,
+) -> Result<()> {
+    let ix = stake::instruction::authorize(
+        ctx.accounts.stake.key,
+        ctx.accounts.authorized.key,
+        ctx.accounts.new_authorized.key,
+        stake_authorize,
+        custodian.as_ref().map(|c| c.key),
+    );
+    let mut account_infos = vec![
+        ctx.accounts.stake,
+        ctx.accounts.clock,
+        ctx.accounts.authorized,
+    ];
+    if let Some(c) = custodian {
+        account_infos.push(c);
+    }
+    solana_program::program::invoke_signed(&ix, &account_infos, ctx.signer_seeds)
+        .map_err(Into::into)
+}
+
+// CPI accounts
+
+#[derive(Accounts)]
+pub struct Authorize<'info> {
+    /// The stake account to be updated
+    pub stake: AccountInfo<'info>,
+
+    /// The existing authority
+    pub authorized: AccountInfo<'info>,
+
+    /// The new authority to replace the existing authority
+    pub new_authorized: AccountInfo<'info>,
+
+    /// Clock sysvar
+    pub clock: AccountInfo<'info>,
+}
+
+// state
 
 #[derive(Clone)]
 pub struct StakeAccount(StakeState);
